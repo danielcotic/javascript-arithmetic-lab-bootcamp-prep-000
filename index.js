b0278b9e74ac6e32bbb024b1e1bd2d788071a849
Daniel Cotic <d.cotic26@gmail.com>
2017-12-07 15:42:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var a = 5
+var b = 3
+
+function add(){
+  return a + b
+}
+
+function subtract(){
+  return a - b
+}
+
+function multiply(){
+  return a * b
+}
+
+function subtract(){
+  return a - b
+}
+
+function divide(){
+  return a / b
+}
+
+function inc(){
+  var n = 5
+  return n += 1
+}
+
+function dec(){
+  var n = 5
+  return n -= 1
+}

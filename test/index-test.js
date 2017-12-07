46507e48f41b5c4905ddd805ae3015257cb4d18b
Daniel Cotic <d.cotic26@gmail.com>
2017-12-07 15:43:04 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,8 @@
 var a, b
 
 beforeEach(function() {
-  a = 5
-  b = 3
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
 })
 
 it('add(a, b) adds two numbers and returns the result', function() {
